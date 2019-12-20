48ecbe022172bb265fee6f446a457cae49504b2f
Nick Palladino <np398@cornell.edu>
2019-12-20 16:05:49 -0500

    Added login event handlers

--- src/main/java/org/breedinginsight/api/auth/AuthService.java
+++ src/main/java/org/breedinginsight/api/auth/AuthService.java
@@ -1,20 +1,45 @@
 package org.breedinginsight.api.auth;
 
+import io.micronaut.context.event.ApplicationEventListener;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.security.authentication.AuthenticationFailed;
 import io.micronaut.security.authentication.UserDetails;
+import io.micronaut.security.event.LoginFailedEvent;
+import io.micronaut.security.event.LoginSuccessfulEvent;
 import io.micronaut.security.handlers.LoginHandler;
 
-public class AuthService /*implements LoginHandler*/ {
+import javax.inject.Singleton;
 
-//    @Override
-//    public HttpResponse loginSuccess(UserDetails userDetails, HttpRequest<?> request) {
-//        return null;
-//    }
-//
-//    @Override
-//    public HttpResponse loginFailed(AuthenticationFailed authenticationFailed) {
-//        return null;
-//    }
+/*
+public class AuthService implements LoginHandler {
+
+    @Override
+    public HttpResponse loginSuccess(UserDetails userDetails, HttpRequest<?> request) {
+        return null;
+    }
+
+    @Override
+    public HttpResponse loginFailed(AuthenticationFailed authenticationFailed) {
+        return null;
+    }
+}
+*/
+
+@Singleton
+class LoginSuccessfulEventListener implements ApplicationEventListener<LoginSuccessfulEvent> {
+    @Override
+    public void onApplicationEvent(LoginSuccessfulEvent event) {
+        UserDetails user = (UserDetails)event.getSource();
+        System.out.println("LoginSuccessfulEvent: User = " + user.getUsername());
+    }
+}
+
+@Singleton
+class LoginFailedEventListener implements ApplicationEventListener<LoginFailedEvent> {
+    @Override
+    public void onApplicationEvent(LoginFailedEvent event) {
+        AuthenticationFailed failed = (AuthenticationFailed)event.getSource();
+        System.out.println("LoginFailedEvent: " + failed.getMessage());
+    }
 }
