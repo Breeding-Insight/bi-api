1d3bd5c437dbc31b43f7b524e741cc2e52adc566
Nick Palladino <np398@cornell.edu>
2020-01-02 15:50:08 -0500

    Login handler is called now

--- src/main/java/org/breedinginsight/api/auth/AuthService.java
+++ src/main/java/org/breedinginsight/api/auth/AuthService.java
@@ -1,45 +1,37 @@
 package org.breedinginsight.api.auth;
 
-import io.micronaut.context.event.ApplicationEventListener;
+import io.micronaut.context.annotation.Replaces;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.security.authentication.AuthenticationFailed;
 import io.micronaut.security.authentication.UserDetails;
-import io.micronaut.security.event.LoginFailedEvent;
-import io.micronaut.security.event.LoginSuccessfulEvent;
-import io.micronaut.security.handlers.LoginHandler;
+import io.micronaut.security.token.jwt.cookie.JwtCookieConfiguration;
+import io.micronaut.security.token.jwt.cookie.JwtCookieLoginHandler;
+import io.micronaut.security.token.jwt.generator.AccessRefreshTokenGenerator;
+import io.micronaut.security.token.jwt.generator.JwtGeneratorConfiguration;
 
 import javax.inject.Singleton;
 
-/*
-public class AuthService implements LoginHandler {
+@Replaces(JwtCookieLoginHandler.class)
+@Singleton
+public class AuthService extends JwtCookieLoginHandler {
+
+    public AuthService(JwtCookieConfiguration jwtCookieConfiguration,
+                       JwtGeneratorConfiguration jwtGeneratorConfiguration,
+                       AccessRefreshTokenGenerator accessRefreshTokenGenerator) {
+        super(jwtCookieConfiguration, jwtGeneratorConfiguration, accessRefreshTokenGenerator);
+    }
 
     @Override
     public HttpResponse loginSuccess(UserDetails userDetails, HttpRequest<?> request) {
-        return null;
+        System.out.println("loginSuccess: User = " + userDetails.getUsername());
+        HttpResponse response = super.loginSuccess(userDetails, request);
+        return response;
     }
 
     @Override
     public HttpResponse loginFailed(AuthenticationFailed authenticationFailed) {
-        return null;
-    }
-}
-*/
-
-@Singleton
-class LoginSuccessfulEventListener implements ApplicationEventListener<LoginSuccessfulEvent> {
-    @Override
-    public void onApplicationEvent(LoginSuccessfulEvent event) {
-        UserDetails user = (UserDetails)event.getSource();
-        System.out.println("LoginSuccessfulEvent: User = " + user.getUsername());
-    }
-}
-
-@Singleton
-class LoginFailedEventListener implements ApplicationEventListener<LoginFailedEvent> {
-    @Override
-    public void onApplicationEvent(LoginFailedEvent event) {
-        AuthenticationFailed failed = (AuthenticationFailed)event.getSource();
-        System.out.println("LoginFailedEvent: " + failed.getMessage());
+        System.out.println("loginFailed");
+        return super.loginFailed(authenticationFailed);
     }
 }
