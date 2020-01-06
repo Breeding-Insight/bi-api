5bdc2027ae8580618c9a5b3941a7da08e4e9ded0
Nick Palladino <np398@cornell.edu>
2020-01-06 13:16:03 -0500

    Added userinfo placeholder

---
+++ src/main/java/org/breedinginsight/api/controller/User.java
@@ -0,0 +1,18 @@
+package org.breedinginsight.api.controller;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
+
+@Controller
+public class User {
+    @Get("/userinfo")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Secured(SecurityRule.IS_ANONYMOUS)
+    public String index() {
+        return "userinfo";
+    }
+}
