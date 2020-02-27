df65f8781b2f33a0d66d19e6ef1f50ff07600826
dmeidlin <git_hub@nym.hush.com>
2020-02-27 11:42:10 -0500

    fix path to application-dev.yml build script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-./src/main/resources/makeApplication-prod.yml.sh
+./src/build/makeApplication-dev.yml.sh
 
 mvn validate flyway:migrate -X
 
 # create a jar file for br-api.
-mvn clean install
+#mvn clean install
+mvn clean install -Dmaven.test.skip=true
 
 # start up the api server
-cd target && java --enable-preview -Dmicronaut.environments=prod -jar bi-api*.jar
+cd target && java --enable-preview -Dmicronaut.environments=dev -jar bi-api*.jar
+#cd target && java --enable-preview -jar bi-api*.jar
