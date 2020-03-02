246a1846d1688c881eff146ec4d79238ec0b8938
Chris T <ct447@cornell.edu>
2020-03-02 10:56:44 -0500

    committing work in progress

--- src/main/java/org/breedinginsight/daos/ProgramDao.java
+++ src/main/java/org/breedinginsight/daos/ProgramDao.java
@@ -1,15 +1,67 @@
 package org.breedinginsight.daos;
 
+import org.breedinginsight.dao.db.tables.pojos.ProgramEntity;
+import org.breedinginsight.dao.db.tables.pojos.SpeciesEntity;
+import org.breedinginsight.dao.db.tables.records.ProgramRecord;
+import org.breedinginsight.model.Program;
 import org.jooq.Configuration;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.breedinginsight.dao.db.Tables.*;
 
 @Singleton
 public class ProgramDao extends org.breedinginsight.dao.db.tables.daos.ProgramDao {
     @Inject
+    DSLContext dsl;
+    @Inject
     public ProgramDao(Configuration config) {
         super(config);
+    }
+
+    public List<Program> getAll(UUID programId){
+
+        Result<Record> queryResults = dsl.select()
+            .from(PROGRAM)
+            .join(SPECIES).on(PROGRAM.SPECIES_ID.eq(SPECIES.ID))
+            .leftJoin(BI_USER).on(PROGRAM.CREATED_BY.eq(BI_USER.ID))
+            .leftJoin(BI_USER).on(PROGRAM.UPDATED_BY.eq(BI_USER.ID))
+            .where(PROGRAM.ID.eq(programId))
+            .fetch();
+
+        for (Record record: queryResults){
+
+            // Generate our program record
+            Program program = new Program();
+            program.setId(record.getValue(PROGRAM.ID));
+            program.setName(record.getValue(PROGRAM.NAME));
+            program.setAbbreviation(record.getValue(PROGRAM.ABBREVIATION));
+            program.setObjective(record.getValue(PROGRAM.OBJECTIVE));
+            program.setDocumentationUrl(record.getValue(PROGRAM.DOCUMENTATION_URL));
+            program.setCreatedAtUtc(record.getValue(PROGRAM.CREATED_AT_UTC));
+            program.setUpdatedAtUtc(record.getValue(PROGRAM.UPDATED_AT_UTC));
+            program.setCreatedBy(record.getValue(PROGRAM.CREATED_BY));
+            program.setUpdatedBy(record.getValue(PROGRAM.UPDATED_BY));
+
+            // Generate our species to attach to our program
+            SpeciesEntity speciesEntity = new SpeciesEntity();
+            speciesEntity.setId(record.getValue(SPECIES.ID));
+            speciesEntity.setCommonName(record.getValue(SPECIES.COMMON_NAME));
+            program.setSpecies(speciesEntity);
+
+            // Generate our created by user to attach to our program
+
+            // Generate our updated by user to attach to our program
+        }
+
+        return new ArrayList<>();
     }
 }
 
--- src/main/java/org/breedinginsight/model/Program.java
+++ src/main/java/org/breedinginsight/model/Program.java
@@ -1,6 +1,33 @@
 package org.breedinginsight.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
 import org.breedinginsight.dao.db.tables.pojos.ProgramEntity;
+import org.breedinginsight.dao.db.tables.pojos.SpeciesEntity;
 
+@Getter
+@Setter
+@Accessors(chain=true)
+@ToString
+@NoArgsConstructor
 public class Program extends ProgramEntity {
+    private SpeciesEntity species;
+
+    public Program(ProgramEntity programEntity){
+
+        this.setId(programEntity.getId());
+        this.setSpeciesId(programEntity.getSpeciesId());
+        this.setName(programEntity.getName());
+        this.setAbbreviation(programEntity.getAbbreviation());
+        this.setObjective(programEntity.getObjective());
+        this.setDocumentationUrl(programEntity.getDocumentationUrl());
+        this.setCreatedAtUtc(programEntity.getCreatedAtUtc());
+        this.setUpdatedAtUtc(programEntity.getUpdatedAtUtc());
+        this.setCreatedBy(programEntity.getCreatedBy());
+        this.setUpdatedBy(programEntity.getUpdatedBy());
+
+    }
 }
