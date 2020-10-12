35a555b4efdd547d5de15fe6e4cc3085394dde9f
Chris T <ct447@cornell.edu>
2020-10-12 14:54:35 -0400

    progress on adding brapi program sync

--- src/main/java/org/breedinginsight/daos/ProgramDAO.java
+++ src/main/java/org/breedinginsight/daos/ProgramDAO.java
@@ -17,6 +17,18 @@
 
 package org.breedinginsight.daos;
 
+import io.micronaut.context.annotation.Property;
+import io.micronaut.http.server.exceptions.HttpServerException;
+import io.micronaut.http.server.exceptions.InternalServerException;
+import org.brapi.client.v2.model.exceptions.APIException;
+import org.brapi.client.v2.model.exceptions.HttpException;
+import org.brapi.client.v2.modules.core.ProgramsAPI;
+import org.brapi.client.v2.modules.phenotype.VariablesAPI;
+import org.brapi.v2.core.model.BrApiExternalReference;
+import org.brapi.v2.core.model.BrApiProgram;
+import org.brapi.v2.core.model.request.ProgramsRequest;
+import org.brapi.v2.phenotyping.model.BrApiVariable;
+import org.breedinginsight.api.model.v1.request.ProgramRequest;
 import org.breedinginsight.dao.db.tables.BiUserTable;
 import org.breedinginsight.dao.db.tables.daos.ProgramDao;
 import org.breedinginsight.dao.db.tables.pojos.ProgramEntity;
@@ -24,12 +36,15 @@ import org.breedinginsight.model.Program;
 import org.breedinginsight.model.ProgramBrAPIEndpoints;
 import org.breedinginsight.model.Species;
 import org.breedinginsight.model.User;
+import org.breedinginsight.services.brapi.BrAPIClientType;
+import org.breedinginsight.services.brapi.BrAPIProvider;
 import org.jooq.*;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.breedinginsight.dao.db.Tables.*;
@@ -38,10 +53,15 @@ import static org.breedinginsight.dao.db.Tables.*;
 public class ProgramDAO extends ProgramDao {
 
     private DSLContext dsl;
+    private BrAPIProvider brAPIProvider;
+    @Property(name = "brapi.server.reference-source")
+    private String referenceSource;
+
     @Inject
-    public ProgramDAO(Configuration config, DSLContext dsl) {
+    public ProgramDAO(Configuration config, DSLContext dsl, BrAPIProvider brAPIProvider) {
         super(config);
         this.dsl = dsl;
+        this.brAPIProvider = brAPIProvider;
     }
 
     public List<Program> get(List<UUID> programIds){
@@ -103,5 +123,65 @@ public class ProgramDAO extends ProgramDao {
     public ProgramBrAPIEndpoints getProgramBrAPIEndpoints() {
         return new ProgramBrAPIEndpoints();
     }
+
+    public void createProgramBrAPI(Program program, User actingUser) {
+
+        BrApiExternalReference externalReference = BrApiExternalReference.builder()
+                .referenceID(program.getId().toString())
+                .referenceSource(referenceSource)
+                .build();
+
+        BrApiProgram brApiProgram = BrApiProgram.builder()
+                .programName(program.getName())
+                .abbreviation(program.getAbbreviation())
+                .commonCropName(program.getSpecies().getCommonName())
+                .externalReferences(List.of(externalReference))
+                .objective(program.getObjective())
+                .build();
+
+        // POST programs to each brapi service
+        // TODO: If there is a failure after the first brapi service, roll back all before the failure.
+        try {
+            List<ProgramsAPI> programsAPIS = brAPIProvider.getAllUniqueProgramsAPI();
+            for (ProgramsAPI programsAPI: programsAPIS){
+                programsAPI.createProgram(brApiProgram);
+            }
+        } catch (HttpException | APIException e) {
+            throw new InternalServerException(e.getMessage());
+        }
+
+    }
+
+    public void updateProgramBrAPI(Program program, User actingUser) {
+
+        ProgramsAPI programsAPI = brAPIProvider.getProgramsAPI(BrAPIClientType.CORE);
+
+        ProgramsRequest searchRequest = ProgramsRequest.builder()
+                .externalReferenceID(program.getId().toString())
+                .externalReferenceSource(referenceSource)
+                .build();
+
+        List<BrApiProgram> brApiProgram = new ArrayList<>();
+        try {
+            brApiProgram = programsAPI.getPrograms(searchRequest);
+        } catch (HttpException | APIException e) {
+            throw new HttpServerException("Could not find program in BrAPI service.");
+        }
+
+        // Get BrAPI program by external reference id
+        BrApiExternalReference externalReference = BrApiExternalReference.builder()
+                .referenceID(program.getId().toString())
+                .referenceSource(referenceSource)
+                .build();
+
+        BrApiProgram brApiProgram = BrApiProgram.builder()
+                .programName(program.getName())
+                .abbreviation(program.getAbbreviation())
+                .commonCropName(program.getSpecies().getCommonName())
+                .externalReferences(List.of(externalReference))
+                .objective(program.getObjective())
+                .build();
+    }
+
 }
 
