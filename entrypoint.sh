62a0ee67042370578e064fd7bf3895093cbacf1f
dmeidlin <git_hub@nym.hush.com>
2020-12-09 12:44:42 -0500

    change the entrypoint so that command line args for build can be passed

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,5 +24,6 @@ mvn validate flyway:migrate -X --settings settings.xml
 mvn clean install -Dmaven.test.skip=true --settings settings.xml -X
 
 # start up the api server
-cd target && java --enable-preview -Dmicronaut.environments=prod -jar bi-api*.jar
+cd target && exec $@
+#cd target && java --enable-preview -Dmicronaut.environments=prod -jar bi-api*.jar
 #cd target && java --enable-preview -jar bi-api*.jar
