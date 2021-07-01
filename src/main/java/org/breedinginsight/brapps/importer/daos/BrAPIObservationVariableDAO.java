542ee71788422ae86340b750f9b560c96a930227
Nick Palladino <np398@cornell.edu>
2021-07-01 10:29:19 -0400

    Change from pheno to core client

--- src/main/java/org/breedinginsight/brapps/importer/daos/BrAPIObservationVariableDAO.java
+++ src/main/java/org/breedinginsight/brapps/importer/daos/BrAPIObservationVariableDAO.java
@@ -42,7 +42,7 @@ public class BrAPIObservationVariableDAO {
         BrAPIObservationVariableSearchRequest variableSearch = new BrAPIObservationVariableSearchRequest();
         variableSearch.observationVariableNames(variableNames);
         variableSearch.setPageSize(BrAPIDAOUtil.RESULTS_PER_QUERY);
-        ObservationVariablesApi api = new ObservationVariablesApi(programDAO.getPhenoClient(programId));
+        ObservationVariablesApi api = new ObservationVariablesApi(programDAO.getCoreClient(programId));
         return BrAPIDAOUtil.search(
                 api::searchVariablesPost,
                 api::searchVariablesSearchResultsDbIdGet,
