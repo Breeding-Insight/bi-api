b6cd83ce9de40063d6891db0a5bbda0a9b46c07a
HMS17 <84345306+HMS17@users.noreply.github.com>
2021-08-05 08:47:37 -0400

    Removing failing logging line

--- src/main/java/org/breedinginsight/daos/ProgramDAO.java
+++ src/main/java/org/breedinginsight/daos/ProgramDAO.java
@@ -240,7 +240,6 @@ public class ProgramDAO extends ProgramDao {
         } catch (ApiException e) {
             log.debug(e.getMessage());
             log.debug(e.getResponseBody());
-            log.debug(e.getCode());
             throw new InternalServerException("Error making BrAPI call", e);
         }
 
