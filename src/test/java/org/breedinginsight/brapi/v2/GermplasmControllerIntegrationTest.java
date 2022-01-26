03812203615eda6b9e06ad39217e22d75ed18540
Chris T <ct447@cornell.edu>
2022-01-26 15:33:37 -0500

    germplasm import: fix germplasm tests

--- src/test/java/org/breedinginsight/brapi/v2/GermplasmControllerIntegrationTest.java
+++ src/test/java/org/breedinginsight/brapi/v2/GermplasmControllerIntegrationTest.java
@@ -9,6 +9,7 @@ import io.micronaut.http.client.annotation.Client;
 import io.micronaut.http.netty.cookies.NettyCookie;
 import io.micronaut.test.annotation.MicronautTest;
 import io.reactivex.Flowable;
+import junit.framework.AssertionFailedError;
 import lombok.SneakyThrows;
 import org.breedinginsight.BrAPITest;
 import org.breedinginsight.TestUtils;
@@ -27,6 +28,7 @@ import javax.inject.Inject;
 
 import java.io.File;
 import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.Map;
 
 import static io.micronaut.http.HttpRequest.GET;
@@ -197,7 +199,7 @@ public class GermplasmControllerIntegrationTest extends BrAPITest {
                 GET(String.format("/programs/%s/brapi/v2/lists",validProgram.getId().toString()))
                         .cookie(new NettyCookie("phylo-token", "test-registered-user")), String.class
         );
-        
+
         HttpResponse<String> response = call.blockingFirst();
         assertEquals(HttpStatus.OK, response.getStatus());
 
@@ -206,10 +208,21 @@ public class GermplasmControllerIntegrationTest extends BrAPITest {
         JsonArray data = result.getAsJsonArray("data");
 
         assertEquals(2, data.size(), "Wrong number of germplasm lists were returned");
-        JsonObject exampleGermplasmList = data.get(0).getAsJsonObject();
-        assertEquals(exampleGermplasmList.get("listName").getAsString(), germplasmListName, "Germplasm list name incorrect");
-        assertEquals(exampleGermplasmList.get("listDescription").getAsString(), germplasmListDesc, "Germplasm list description incorrect");
-        assertEquals(exampleGermplasmList.get("listSize").getAsInt(), 3, "Germplasm list displays incorrect total entries ");
+        List<String> listNames = List.of(germplasmListName, "Program List1");
+        List<String> listDescription = List.of(germplasmListDesc, "Program List1");
+        for (JsonElement element: data) {
+            JsonObject exampleGermplasmList = element.getAsJsonObject();
+            if (listNames.contains(exampleGermplasmList.get("listName").getAsString())) {
+                Integer ind = listNames.indexOf(exampleGermplasmList.get("listName").getAsString());
+                assertEquals(exampleGermplasmList.get("listName").getAsString(), listNames.get(ind), "Germplasm list name incorrect");
+                assertEquals(exampleGermplasmList.get("listDescription").getAsString(), listDescription.get(ind), "Germplasm list description incorrect");
+                if (ind == 0) {
+                    assertEquals(exampleGermplasmList.get("listSize").getAsInt(), 3, "Germplasm list displays incorrect total entries ");
+                }
+            } else {
+                throw new AssertionFailedError("List name not found");
+            }
+        }
     }
 
 }
