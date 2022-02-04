c3438552d3bf4e8e812f1cd2219a92c82104968d
dmeidlin <14339308+dmeidlin@users.noreply.github.com>
2022-02-04 16:15:47 -0500

    undo dummy commit

--- src/main/java/org/breedinginsight/daos/TraitDAO.java
+++ src/main/java/org/breedinginsight/daos/TraitDAO.java
@@ -294,7 +294,6 @@ public class TraitDAO extends TraitDao {
             if (!brApiSynonyms.contains(trait.getFullName())) {
                 brApiSynonyms.add(trait.getFullName());
             }
-            //
             BrAPIExternalReference traitReference = new BrAPIExternalReference()
                     .referenceID(trait.getId().toString())
                     .referenceSource(referenceSource);
