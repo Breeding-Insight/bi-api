a309419ebbe2053cd7d5927ec7b7fb321573f6cf
HMS17 <84345306+HMS17@users.noreply.github.com>
2022-06-23 12:45:26 -0400

    [BI-1224] - Show list creator in Germplasm lists table

--- src/main/java/org/breedinginsight/brapi/v2/services/BrAPIGermplasmService.java
+++ src/main/java/org/breedinginsight/brapi/v2/services/BrAPIGermplasmService.java
@@ -83,6 +83,18 @@ public class BrAPIGermplasmService {
                 String listName = germplasmList.getListName();
                 String newListName = removeAppendedKey(listName, program.getKey());
                 germplasmList.setListName(newListName);
+
+                //Retrieve germplasm details to get list owner name
+                BrAPIListDetails listData = brAPIListDAO.getListById(germplasmList.getListDbId(), programId).getResult();
+                List<String> germplasmNames = listData.getData().subList(0,1);
+                List<BrAPIGermplasm> germplasm = germplasmDAO.getGermplasmByRawName(germplasmNames, programId);
+                String createdBy = germplasm.get(0).getAdditionalInfo().getAsJsonObject("createdBy").get("userName").getAsString();
+                germplasmList.setListOwnerName(createdBy);
+
+                //germplasmList.getListType(); value germplasm name GERMPLASM
+                //germplasmList.getAdditionalInfo(); todo utilize this instead if possible
+                //logic based on listType to know what BrAPI endpoint to call after fetching the first record in the list
+                //in order to fetch the full object
             }
 
             return germplasmLists;
