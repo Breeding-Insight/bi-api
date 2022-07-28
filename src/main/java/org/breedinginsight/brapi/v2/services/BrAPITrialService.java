3b4d3d0b74e19f85c9e2b1317e6ae57db0728f81
HMS17 <84345306+HMS17@users.noreply.github.com>
2022-07-28 13:48:24 -0400

    [BI-1198] All Experiments Table

---
+++ src/main/java/org/breedinginsight/brapi/v2/services/BrAPITrialService.java
@@ -0,0 +1,39 @@
+package org.breedinginsight.brapi.v2.services;
+
+import io.micronaut.context.annotation.Property;
+import io.micronaut.http.server.exceptions.InternalServerException;
+import org.brapi.client.v2.model.exceptions.ApiException;
+import lombok.extern.slf4j.Slf4j;
+import org.brapi.v2.model.core.BrAPITrial;
+import org.breedinginsight.brapps.importer.daos.BrAPITrialDAO;
+import org.breedinginsight.services.ProgramService;
+import org.breedinginsight.services.exceptions.DoesNotExistException;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.*;
+
+@Slf4j
+@Singleton
+public class BrAPITrialService {
+
+    @Property(name = "brapi.server.reference-source")
+    private String referenceSource;
+
+    private final BrAPITrialDAO trialDAO;
+    private final ProgramService programService;
+
+    @Inject
+    public BrAPITrialService(ProgramService programService, BrAPITrialDAO trialDAO) {
+        this.programService = programService;
+        this.trialDAO = trialDAO;
+    }
+
+    public List<BrAPITrial> getExperiments(UUID programId) throws ApiException {
+        try {
+            return trialDAO.getTrials(programId);
+        } catch (ApiException | DoesNotExistException e) {
+            throw new InternalServerException(e.getMessage(), e);
+        }
+    }
+}
