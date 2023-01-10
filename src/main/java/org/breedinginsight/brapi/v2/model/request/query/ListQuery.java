a666c46c836e65828efc3a54266e1d7b3a4876b0
Nick Palladino <np398@cornell.edu>
2023-01-10 15:02:32 -0500

    Add missing parameters to search request

--- src/main/java/org/breedinginsight/brapi/v2/model/request/query/ListQuery.java
+++ src/main/java/org/breedinginsight/brapi/v2/model/request/query/ListQuery.java
@@ -16,11 +16,31 @@ import java.util.List;
 @Introspected
 public class ListQuery extends BrapiQuery {
     private String listType;
+    private String name;
+    private String description;
+    private String size;
+    private String dateCreated;
+    private String ownerName;
 
     public SearchRequest constructSearchRequest() {
         List<FilterRequest> filters = new ArrayList<>();
         if (!StringUtils.isBlank(getListType())) {
             filters.add(constructFilterRequest("type", getListType()));
+        }
+        if (!StringUtils.isBlank(getName())) {
+            filters.add(constructFilterRequest("name", getName()));
+        }
+        if (!StringUtils.isBlank(getDescription())) {
+            filters.add(constructFilterRequest("description", getDescription()));
+        }
+        if (!StringUtils.isBlank(getSize())) {
+            filters.add(constructFilterRequest("size", getSize()));
+        }
+        if (!StringUtils.isBlank(getDateCreated())) {
+            filters.add(constructFilterRequest("dateCreated", getDateCreated()));
+        }
+        if (!StringUtils.isBlank(getOwnerName())) {
+            filters.add(constructFilterRequest("ownerName", getOwnerName()));
         }
         return new SearchRequest(filters);
     }
