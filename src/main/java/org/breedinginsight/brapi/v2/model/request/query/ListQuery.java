db7f402011afcca5dda70578ab74a3f9862acdc0
dmeidlin <14339308+dmeidlin@users.noreply.github.com>
2023-06-20 12:39:15 -0400

    Update ListQuery to include xrefsource and xrefid

--- src/main/java/org/breedinginsight/brapi/v2/model/request/query/ListQuery.java
+++ src/main/java/org/breedinginsight/brapi/v2/model/request/query/ListQuery.java
@@ -17,6 +17,8 @@ public class ListQuery extends BrapiQuery {
     private String name;
     private String description;
     private String size;
+    private String externalReferenceSource;
+    private String externalReferenceId;
     private String dateCreated;
     private String ownerName;
     // This is a meta-parameter, it describes the display format of any date fields.
@@ -35,6 +37,12 @@ public class ListQuery extends BrapiQuery {
         }
         if (!StringUtils.isBlank(getSize())) {
             filters.add(constructFilterRequest("size", getSize()));
+        }
+        if (!StringUtils.isBlank(getExternalReferenceSource())) {
+            filters.add(constructFilterRequest("externalReferenceSource", getExternalReferenceSource()));
+        }
+        if (!StringUtils.isBlank(getExternalReferenceId())) {
+            filters.add(constructFilterRequest("externalReferenceId", getExternalReferenceId()));
         }
         if (!StringUtils.isBlank(getDateCreated())) {
             filters.add(constructFilterRequest("dateCreated", getDateCreated()));
