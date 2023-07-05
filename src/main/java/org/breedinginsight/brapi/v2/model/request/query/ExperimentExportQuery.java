a95e95fdda1a162416df8cd3a519fe0dd05b7fc7
mlm483 <128052931+mlm483@users.noreply.github.com>
2023-07-05 16:04:39 -0400

    [BI-1720] - cleaned up ExperimentExportQuery.java

--- src/main/java/org/breedinginsight/brapi/v2/model/request/query/ExperimentExportQuery.java
+++ src/main/java/org/breedinginsight/brapi/v2/model/request/query/ExperimentExportQuery.java
@@ -2,23 +2,15 @@ package org.breedinginsight.brapi.v2.model.request.query;
 
 import io.micronaut.core.annotation.Introspected;
 import lombok.Getter;
-import lombok.Setter;
-import org.breedinginsight.api.model.v1.request.query.FilterRequest;
-import org.breedinginsight.api.model.v1.request.query.SearchRequest;
-import org.breedinginsight.brapi.v1.model.request.query.BrapiQuery;
 import org.breedinginsight.brapps.importer.model.exports.FileType;
-import org.jooq.tools.StringUtils;
 
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Introspected
 public class ExperimentExportQuery {
     private FileType fileExtension;
     private String dataset;
-    @Setter
     private String environments;
     @NotNull
     private boolean includeTimestamps;
