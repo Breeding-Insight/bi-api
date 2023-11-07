921fd0856e1d627fe144c88d99816617337820ba
David Randolph Phillips <drp227@cornell.edu>
2023-11-07 12:18:06 -0500

    [BI-1914] changed Integer to BigInteger Changed the wording from 'integers' to 'digits'

--- src/main/java/org/breedinginsight/utilities/IntOrderComparator.java
+++ src/main/java/org/breedinginsight/utilities/IntOrderComparator.java
@@ -1,5 +1,6 @@
 package org.breedinginsight.utilities;
 
+import java.math.BigInteger;
 import java.util.Comparator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -8,24 +9,23 @@ If you have text(optional), followed by some digits(optional), followed by more 
 will sort the digits in numeric order.  The trailing text (and any subsequent digits) will be in
 alpha-numeric order.
 
-In other words; this comparator will assume that strings being compared are comprised of a text _prefix_, and an
-_integer_, followed by a _suffix_.
+In other words; this comparator will assume that strings being compared are comprised of a text _prefix_, followed by
+_digits_, followed by a _suffix_.
 
-This comparator will fist compare the prefix as an alpha-numeric, the integer as numeric and
+This comparator will fist compare the prefix as an alpha-numeric, the digits as a numeric and
 then the the suffix as an alpha-numeric.
 For Example:
-1) if a string is 'big4team', then prefix='big', integer=4, suffix='team'
-2) if a string is '12monkeys', then prefix='', integer=12, suffix='monkeys'
-3) if a string is 'abcd', then prefix='abcd', integer=null, suffix=''
-4) if a string is 'libnum14.3', then prefix='libnum', integer=14, suffix='.4'
-
+1) if a string is 'big4team', then prefix='big', digits=4, suffix='team'
+2) if a string is '12monkeys', then prefix='', digits=12, suffix='monkeys'
+3) if a string is 'abcd', then prefix='abcd', digits=null, suffix=''
+4) if a string is 'libnum14.3', then prefix='libnum', digits=14, suffix='.4'
 */
 public class IntOrderComparator implements Comparator<String> {
     @Override
     public int compare(String str1, String str2) {
         //static finals to make the Matcher::group code more readable
         final int PREFIX = 1;
-        final int INTEGER = 2;
+        final int DIGITS = 2;
         final int SUFFIX = 3;
 
         // convert null strings to blank
@@ -45,31 +45,32 @@ public class IntOrderComparator implements Comparator<String> {
 
         String prefix1 = m1.group(PREFIX);
         String prefix2 = m2.group(PREFIX);
-        Integer integer1 = m1.group(INTEGER).length() > 0 ? Integer.parseInt(m1.group(INTEGER)) : null;
-        Integer integer2 = m2.group(INTEGER).length() > 0 ? Integer.parseInt(m2.group(INTEGER)) : null;
+        BigInteger digits1 = m1.group(DIGITS).length() > 0 ? new BigInteger(m1.group(DIGITS)) : null;
+        BigInteger digits2 = m2.group(DIGITS).length() > 0 ? new BigInteger(m2.group(DIGITS)) : null;
         String suffix1 = m1.group(SUFFIX);
         String suffix2 = m2.group(SUFFIX);
 
         if (!prefix1.equals(prefix2)) {
             return prefix1.compareTo(prefix2);
         }
-        /*if the prefixes are equal, sort by integers and suffixes.*/
+        /*if the prefixes are equal, sort by digits and suffixes.*/
 
-        // an empty integer goes before an integer (EX. 'a' before 'a1')
-        if (integer1 == null && integer2 != null) {
-            return -1;
+        // an empty digit is less than any digit (EX. 'a' is less than 'a1')
+        if( digits1 == null){
+            if(digits2 == null){
+                return 0;  //if both digits are null (equal)
+            }
+            else {
+                return -1; //if there are no digits in str1 but digits in str2 (str1 < str2)
+            }
         }
-        if (integer1 != null && integer2 == null) {
-            return 1;
+        if( digits2 == null){
+            return 1; //if there are digits in str1 but no digits in str2 (str1 > str2)
         }
-        if (integer1 == null && integer2 == null) {
-            return 0;
+        if (!digits1.equals(digits2)) {
+            return digits1.compareTo(digits2);
         }
-
-        if (!integer1.equals(integer2)) {
-            return integer1.compareTo(integer2);
-        }
-        /*if the prefixes and integers are equal, sort by suffixes.*/
+        /*if the prefixes and digits are equal, sort by suffixes.*/
         if (!suffix1.equals(suffix2)) {
             return suffix1.compareTo(suffix2);
         }
