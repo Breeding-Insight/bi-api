314d0b129d6f563b2b2bf34cbebe51f8fb7bf0a5
Nick <53413353+nickpalladino@users.noreply.github.com>
2024-05-06 17:07:01 -0400

    Added db migration to create workflows

---
+++ src/main/resources/db/migration/V1.22.0__add_experiment_workflows.sql
@@ -0,0 +1,41 @@
+/*
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+CREATE TABLE importer_mapping_workflow
+(
+    like base_entity INCLUDING DEFAULTS INCLUDING CONSTRAINTS INCLUDING INDEXES,
+    mapping_id      UUID NOT NULL,
+    name            TEXT
+);
+
+ALTER TABLE importer_mapping_workflow
+    ADD FOREIGN KEY (mapping_id) REFERENCES importer_mapping (id);
+
+DO
+$$
+DECLARE
+  exp_mapping_id UUID;
+BEGIN
+  exp_mapping_id := (SELECT id FROM importer_mapping WHERE name = 'ExperimentsTemplateMap');
+
+INSERT INTO public.importer_mapping_workflow (mapping_id, name)
+VALUES
+    (exp_mapping_id, 'Create new experiment'),
+    (exp_mapping_id, 'Append experimental dataset'),
+    (exp_mapping_id, 'Create new experimental environment');
+END
+$$;
