fa8c7cb586f4bf4110a505e10e090fecee0cd714
mlm483 <128052931+mlm483@users.noreply.github.com>
2024-05-15 13:11:44 -0400

    [BI-2128] fixed typo

--- src/test/java/org/breedinginsight/brapps/importer/ExperimentFileImportTest.java
+++ src/test/java/org/breedinginsight/brapps/importer/ExperimentFileImportTest.java
@@ -825,7 +825,7 @@ public class ExperimentFileImportTest extends BrAPITest {
     Scenario:
     - an experiment was created with observations
     - an overwrite operation is attempted with blank observation values
-    - verify blank obervation values do not overwrite original values
+    - verify blank observation values do not overwrite original values
      */
     @ParameterizedTest
     @ValueSource(booleans = {true, false})
