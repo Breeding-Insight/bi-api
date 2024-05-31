402eb5de2dd58f1fa8076db344a88e150a5ed390
dmeidlin <14339308+dmeidlin@users.noreply.github.com>
2024-05-31 10:40:55 -0400

    create DomainImportService abstract class

--- src/main/java/org/breedinginsight/brapps/importer/model/workflow/Workflow.java
+++ src/main/java/org/breedinginsight/brapps/importer/model/workflow/Workflow.java
@@ -3,9 +3,15 @@ package org.breedinginsight.brapps.importer.model.workflow;
 import io.micronaut.core.order.Ordered;
 import org.breedinginsight.brapps.importer.model.imports.ImportServiceContext;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @FunctionalInterface
 public interface Workflow extends Ordered {
     Optional<ImportWorkflowResult> process(ImportServiceContext context);
+    default List<ImportWorkflow> getWorkflows() {
+        // Default implementation for getWorkflows method
+        return new ArrayList<>();
+    }
 }
