49e89bbe1644a32d8a4cb1af1272370b59b89cb9
dmeidlin <14339308+dmeidlin@users.noreply.github.com>
2024-06-11 17:29:42 -0400

    fix injection pending study

--- src/main/java/org/breedinginsight/brapps/importer/services/processors/experiment/appendoverwrite/action/create/BrAPIStudyCreation.java
+++ src/main/java/org/breedinginsight/brapps/importer/services/processors/experiment/appendoverwrite/action/create/BrAPIStudyCreation.java
@@ -1,10 +1,14 @@
 package org.breedinginsight.brapps.importer.services.processors.experiment.appendoverwrite.action.create;
 
+import io.micronaut.context.ApplicationContext;
 import lombok.extern.slf4j.Slf4j;
 import org.brapi.v2.model.core.BrAPIStudy;
+import org.breedinginsight.brapi.v2.dao.BrAPIStudyDAO;
+import org.breedinginsight.brapps.importer.services.processors.experiment.ExperimentUtilities;
 import org.breedinginsight.brapps.importer.services.processors.experiment.appendoverwrite.entity.ExperimentImportEntity;
 import org.breedinginsight.brapps.importer.services.processors.experiment.appendoverwrite.entity.PendingStudy;
 import org.breedinginsight.brapps.importer.services.processors.experiment.model.ExpUnitMiddlewareContext;
+import org.breedinginsight.brapps.importer.services.processors.experiment.service.StudyService;
 
 @Slf4j
 public class BrAPIStudyCreation extends BrAPICreation<BrAPIStudy> {
@@ -21,6 +25,12 @@ public class BrAPIStudyCreation extends BrAPICreation<BrAPIStudy> {
      */
     @Override
     public ExperimentImportEntity<BrAPIStudy> getEntity(ExpUnitMiddlewareContext context) {
-        return new PendingStudy(context);
+        try (ApplicationContext appContext = ApplicationContext.run()) {
+            StudyService studyService = appContext.getBean(StudyService.class);
+            BrAPIStudyDAO brAPIStudyDAO = appContext.getBean(BrAPIStudyDAO.class);
+            ExperimentUtilities experimentUtilities = appContext.getBean(ExperimentUtilities.class);
+
+            return new PendingStudy(context, studyService, brAPIStudyDAO, experimentUtilities);
+        }
     }
 }
--- src/main/java/org/breedinginsight/brapps/importer/services/processors/experiment/appendoverwrite/action/read/BrAPIStudyReadWorkflowInitialization.java
+++ src/main/java/org/breedinginsight/brapps/importer/services/processors/experiment/appendoverwrite/action/read/BrAPIStudyReadWorkflowInitialization.java
@@ -1,9 +1,13 @@
 package org.breedinginsight.brapps.importer.services.processors.experiment.appendoverwrite.action.read;
 
+import io.micronaut.context.ApplicationContext;
 import org.brapi.v2.model.core.BrAPIStudy;
+import org.breedinginsight.brapi.v2.dao.BrAPIStudyDAO;
+import org.breedinginsight.brapps.importer.services.processors.experiment.ExperimentUtilities;
 import org.breedinginsight.brapps.importer.services.processors.experiment.appendoverwrite.entity.ExperimentImportEntity;
 import org.breedinginsight.brapps.importer.services.processors.experiment.appendoverwrite.entity.PendingStudy;
 import org.breedinginsight.brapps.importer.services.processors.experiment.model.ExpUnitMiddlewareContext;
+import org.breedinginsight.brapps.importer.services.processors.experiment.service.StudyService;
 
 public class BrAPIStudyReadWorkflowInitialization extends BrAPIReadWorkflowInitialization<BrAPIStudy> {
 
@@ -19,6 +23,12 @@ public class BrAPIStudyReadWorkflowInitialization extends BrAPIReadWorkflowIniti
      */
     @Override
     public ExperimentImportEntity<BrAPIStudy> getEntity(ExpUnitMiddlewareContext context) {
-        return new PendingStudy(context);
+        try (ApplicationContext appContext = ApplicationContext.run()) {
+            StudyService studyService = appContext.getBean(StudyService.class);
+            BrAPIStudyDAO brAPIStudyDAO = appContext.getBean(BrAPIStudyDAO.class);
+            ExperimentUtilities experimentUtilities = appContext.getBean(ExperimentUtilities.class);
+
+            return new PendingStudy(context, studyService, brAPIStudyDAO, experimentUtilities);
+        }
     }
 }
