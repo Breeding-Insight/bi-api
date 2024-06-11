06ba0388561111f8a8ac07ef08878b4d9f1128cc
dmeidlin <14339308+dmeidlin@users.noreply.github.com>
2024-06-11 18:04:32 -0400

    fix injection pending germplasm

--- src/main/java/org/breedinginsight/brapps/importer/services/processors/experiment/appendoverwrite/action/read/BrAPIGermplasmReadWorkflowInitialization.java
+++ src/main/java/org/breedinginsight/brapps/importer/services/processors/experiment/appendoverwrite/action/read/BrAPIGermplasmReadWorkflowInitialization.java
@@ -1,9 +1,12 @@
 package org.breedinginsight.brapps.importer.services.processors.experiment.appendoverwrite.action.read;
 
+import io.micronaut.context.ApplicationContext;
 import org.brapi.v2.model.germ.BrAPIGermplasm;
+import org.breedinginsight.brapps.importer.services.processors.experiment.ExperimentUtilities;
 import org.breedinginsight.brapps.importer.services.processors.experiment.appendoverwrite.entity.ExperimentImportEntity;
 import org.breedinginsight.brapps.importer.services.processors.experiment.appendoverwrite.entity.PendingGermplasm;
 import org.breedinginsight.brapps.importer.services.processors.experiment.model.ExpUnitMiddlewareContext;
+import org.breedinginsight.brapps.importer.services.processors.experiment.service.GermplasmService;
 
 public class BrAPIGermplasmReadWorkflowInitialization extends BrAPIReadWorkflowInitialization<BrAPIGermplasm> {
     /**
@@ -24,6 +27,11 @@ public class BrAPIGermplasmReadWorkflowInitialization extends BrAPIReadWorkflowI
      */
     @Override
     public ExperimentImportEntity<BrAPIGermplasm> getEntity(ExpUnitMiddlewareContext context) {
-        return new PendingGermplasm(context);
+        try (ApplicationContext appContext = ApplicationContext.run()) {
+            GermplasmService germplasmService = appContext.getBean(GermplasmService.class);
+            ExperimentUtilities experimentUtilities = appContext.getBean(ExperimentUtilities.class);
+
+            return new PendingGermplasm(context, germplasmService, experimentUtilities);
+        }
     }
 }
