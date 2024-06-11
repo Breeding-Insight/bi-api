5dab53d2de9a59d05252e34aa5559e8f29cbb60b
dmeidlin <14339308+dmeidlin@users.noreply.github.com>
2024-06-11 16:43:12 -0400

    fix injection for pending OU creation

--- src/main/java/org/breedinginsight/brapps/importer/services/processors/experiment/appendoverwrite/action/create/BrAPIObservationUnitCreation.java
+++ src/main/java/org/breedinginsight/brapps/importer/services/processors/experiment/appendoverwrite/action/create/BrAPIObservationUnitCreation.java
@@ -1,9 +1,13 @@
 package org.breedinginsight.brapps.importer.services.processors.experiment.appendoverwrite.action.create;
 
+import io.micronaut.context.ApplicationContext;
 import org.brapi.v2.model.pheno.BrAPIObservationUnit;
+import org.breedinginsight.brapi.v2.dao.BrAPIObservationUnitDAO;
+import org.breedinginsight.brapps.importer.services.processors.experiment.ExperimentUtilities;
 import org.breedinginsight.brapps.importer.services.processors.experiment.appendoverwrite.entity.ExperimentImportEntity;
 import org.breedinginsight.brapps.importer.services.processors.experiment.appendoverwrite.entity.PendingObservationUnit;
 import org.breedinginsight.brapps.importer.services.processors.experiment.model.ExpUnitMiddlewareContext;
+import org.breedinginsight.brapps.importer.services.processors.experiment.service.ObservationUnitService;
 
 public class BrAPIObservationUnitCreation extends BrAPICreation<BrAPIObservationUnit> {
     /**
@@ -23,6 +27,12 @@ public class BrAPIObservationUnitCreation extends BrAPICreation<BrAPIObservation
      */
     @Override
     public ExperimentImportEntity<BrAPIObservationUnit> getEntity(ExpUnitMiddlewareContext context) {
-        return new PendingObservationUnit(context);
+        try (ApplicationContext appContext = ApplicationContext.run()) {
+            BrAPIObservationUnitDAO observationUnitDAO = appContext.getBean(BrAPIObservationUnitDAO.class);
+            ObservationUnitService observationUnitService = appContext.getBean(ObservationUnitService.class);
+            ExperimentUtilities experimentUtilities = appContext.getBean(ExperimentUtilities.class);
+
+            return new PendingObservationUnit(context, observationUnitDAO, observationUnitService, experimentUtilities);
+        }
     }
 }
