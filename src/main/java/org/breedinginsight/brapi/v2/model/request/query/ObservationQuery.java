1339fb08c313451a9556bbd2943fce859fa8c8e5
mlm483 <128052931+mlm483@users.noreply.github.com>
2024-10-08 17:12:12 -0400

    [BI-2330] - updated ObservationQuery

--- src/main/java/org/breedinginsight/brapi/v2/model/request/query/ObservationQuery.java
+++ src/main/java/org/breedinginsight/brapi/v2/model/request/query/ObservationQuery.java
@@ -2,10 +2,10 @@ package org.breedinginsight.brapi.v2.model.request.query;
 
 import io.micronaut.core.annotation.Introspected;
 import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
 import org.breedinginsight.api.model.v1.request.query.FilterRequest;
 import org.breedinginsight.api.model.v1.request.query.SearchRequest;
 import org.breedinginsight.brapi.v1.model.request.query.BrapiQuery;
-import org.jooq.tools.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +13,6 @@ import java.util.List;
 @Getter
 @Introspected
 public class ObservationQuery extends BrapiQuery {
-    private String accept;
     private String observationUnitDbId;
     private String observationVariableDbId;
     private String locationDbId;
@@ -35,9 +34,6 @@ public class ObservationQuery extends BrapiQuery {
 
     public SearchRequest constructSearchRequest() {
         List<FilterRequest> filters = new ArrayList<>();
-        if (!StringUtils.isBlank(getAccept())) {
-            filters.add(constructFilterRequest("accept", getAccept()));
-        }
         if (!StringUtils.isBlank(getObservationUnitDbId())) {
             filters.add(constructFilterRequest("observationUnitDbId", getObservationUnitDbId()));
         }
@@ -94,4 +90,73 @@ public class ObservationQuery extends BrapiQuery {
         }
         return new SearchRequest(filters);
     }
+
+    /**
+     * Build a query string from the ObservationQuery object.
+     */
+    public String toQueryString() {
+        StringBuilder queryStringBuilder = new StringBuilder();
+        if (StringUtils.isNotBlank(observationUnitDbId)) {
+            queryStringBuilder.append("observationUnitDbId=").append(observationUnitDbId).append("&");
+        }
+        if (StringUtils.isNotBlank(observationVariableDbId)) {
+            queryStringBuilder.append("observationVariableDbId=").append(observationVariableDbId).append("&");
+        }
+        if (StringUtils.isNotBlank(locationDbId)) {
+            queryStringBuilder.append("locationDbId=").append(locationDbId).append("&");
+        }
+        if (StringUtils.isNotBlank(seasonDbId)) {
+            queryStringBuilder.append("seasonDbId=").append(seasonDbId).append("&");
+        }
+        if (StringUtils.isNotBlank(observationLevel)) {
+            queryStringBuilder.append("observationLevel=").append(observationLevel).append("&");
+        }
+        if (StringUtils.isNotBlank(searchResultsDbId)) {
+            queryStringBuilder.append("searchResultsDbId=").append(searchResultsDbId).append("&");
+        }
+        if (StringUtils.isNotBlank(observationTimeStampRangeStart)) {
+            queryStringBuilder.append("observationTimeStampRangeStart=").append(observationTimeStampRangeStart).append("&");
+        }
+        if (StringUtils.isNotBlank(observationTimeStampRangeEnd)) {
+            queryStringBuilder.append("observationTimeStampRangeEnd=").append(observationTimeStampRangeEnd).append("&");
+        }
+        if (StringUtils.isNotBlank(programDbId)) {
+            queryStringBuilder.append("programDbId=").append(programDbId).append("&");
+        }
+        if (StringUtils.isNotBlank(trialDbId)) {
+            queryStringBuilder.append("trialDbId=").append(trialDbId).append("&");
+        }
+        if (StringUtils.isNotBlank(studyDbId)) {
+            queryStringBuilder.append("studyDbId=").append(studyDbId).append("&");
+        }
+        if (StringUtils.isNotBlank(germplasmDbId)) {
+            queryStringBuilder.append("germplasmDbId=").append(germplasmDbId).append("&");
+        }
+        if (StringUtils.isNotBlank(observationUnitLevelName)) {
+            queryStringBuilder.append("observationUnitLevelName=").append(observationUnitLevelName).append("&");
+        }
+        if (StringUtils.isNotBlank(observationUnitLevelOrder)) {
+            queryStringBuilder.append("observationUnitLevelOrder=").append(observationUnitLevelOrder).append("&");
+        }
+        if (StringUtils.isNotBlank(observationUnitLevelCode)) {
+            queryStringBuilder.append("observationUnitLevelCode=").append(observationUnitLevelCode).append("&");
+        }
+        if (StringUtils.isNotBlank(observationUnitLevelRelationshipName)) {
+            queryStringBuilder.append("observationUnitLevelRelationshipName=").append(observationUnitLevelRelationshipName).append("&");
+        }
+        if (StringUtils.isNotBlank(observationUnitLevelRelationshipOrder)) {
+            queryStringBuilder.append("observationUnitLevelRelationshipOrder=").append(observationUnitLevelRelationshipOrder).append("&");
+        }
+        if (StringUtils.isNotBlank(observationUnitLevelRelationshipCode)) {
+            queryStringBuilder.append("observationUnitLevelRelationshipCode=").append(observationUnitLevelRelationshipCode).append("&");
+        }
+
+        // If any query parameters were added, remove the trailing "&" from the query string.
+        if (queryStringBuilder.length() > 0) {
+            queryStringBuilder.deleteCharAt(queryStringBuilder.length() - 1);
+        }
+
+        return queryStringBuilder.toString();
+    }
+
 }
