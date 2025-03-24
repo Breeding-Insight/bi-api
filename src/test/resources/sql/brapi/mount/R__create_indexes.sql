69c82eeacfc82295ec71514d1788077e6bccc1cb
David Randolph Phillips <drp227@cornell.edu>
2025-03-24 09:58:16 -0400

    [BI-2585] fixed type-o in migrations

--- src/test/resources/sql/brapi/mount/R__create_indexes.sql
+++ src/test/resources/sql/brapi/mount/R__create_indexes.sql
@@ -15,7 +15,7 @@
 
 -- Indexes to improve read performance of Germplasm operations.
 CREATE INDEX CONCURRENTLY IF NOT EXISTS "pedigree_edge_this_node_id" ON pedigree_edge (this_node_id);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS "pedigree_edge_connected_node_id" ON pedigree_edge (connceted_node_id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS "pedigree_edge_connected_node_id" ON pedigree_edge (connected_node_id);
 CREATE INDEX CONCURRENTLY IF NOT EXISTS "pedigree_edge_edge_type" ON pedigree_edge (edge_type);
 CREATE INDEX CONCURRENTLY IF NOT EXISTS "program_external_references_program_entity_id" ON program_external_references (program_entity_id);
 CREATE INDEX CONCURRENTLY IF NOT EXISTS "external_reference_composite" ON external_reference (external_reference_source, external_reference_id);
