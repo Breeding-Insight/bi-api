8ebc08023fa521a20d8f3aeb56df20ef415f6e7f
Nick <53413353+nickpalladino@users.noreply.github.com>
2025-06-11 14:38:58 -0400

    Remove comments in test script

--- src/test/resources/sql/brapi/species.sql
+++ src/test/resources/sql/brapi/species.sql
@@ -23,17 +23,9 @@ DO $$
 DECLARE
 v_auth_id CONSTANT uuid := 'AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA';
 BEGIN
-    /* ------------------------------------------------------------------------------------------
-       • uuid_generate_v5(namespace, crop_name)  → deterministic UUID can be used for idempotency
-       • Do it this way so no schema changes are required
-       • Removed the Honey Bee special case because all systems will be starting fresh
-       ------------------------------------------------------------------------------------------ */
 INSERT INTO crop (id, auth_user_id, crop_name)
 SELECT
     uuid_generate_v5('9a4deca9-4068-46a3-9efe-db0c181f491a'::uuid,
-        -- 1) lower‑case
-        -- 2) trim leading/trailing space
-        -- 3) REMOVE every space or tab
                      regexp_replace(lower(trim(crop_name)), '\s', '', 'g')),
     v_auth_id,
     crop_name
@@ -47,8 +39,6 @@ FROM (VALUES
           ('Raspberry'), ('Sugar Beet'), ('Coffee')
      ) AS src(crop_name)
     ON CONFLICT (id) DO
--- want case changes or space changes to overwrite existing
--- Only rewrite the row if name changed
 UPDATE SET crop_name = EXCLUDED.crop_name
 WHERE crop.crop_name IS DISTINCT FROM EXCLUDED.crop_name;
 END $$;
