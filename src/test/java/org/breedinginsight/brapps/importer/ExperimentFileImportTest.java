5379e4a734fea0381e7877655a112c114d1474ec
HMS17 <hms243@cornell.edu>
2026-03-06 10:30:49 -0500

    [BI-2778] Tests casing fix

--- src/test/java/org/breedinginsight/brapps/importer/ExperimentFileImportTest.java
+++ src/test/java/org/breedinginsight/brapps/importer/ExperimentFileImportTest.java
@@ -1700,7 +1700,7 @@ public class ExperimentFileImportTest extends BrAPITest {
         assertEquals(expected.get(Columns.EXP_TITLE), Utilities.removeProgramKey(trial.getTrialName(), program.getKey()));
         assertEquals(expected.get(Columns.EXP_TITLE), Utilities.removeProgramKey(study.getTrialName(), program.getKey()));
         assertEquals(expected.get(Columns.EXP_DESCRIPTION), trial.getTrialDescription());
-        assertEquals(expected.get(Columns.EXP_UNIT), ou.getObservationUnitPosition().getObservationLevel().getLevelName());
+        assertEquals(expected.get(Columns.EXP_UNIT).toString().toLowerCase(), ou.getObservationUnitPosition().getObservationLevel().getLevelName().toLowerCase());
         assertEquals(expected.get(Columns.EXP_TYPE), trial.getAdditionalInfo().get(BrAPIAdditionalInfoFields.EXPERIMENT_TYPE).getAsString());
         assertEquals(expected.get(Columns.EXP_TYPE), study.getStudyType());
         assertEquals(expected.get(Columns.ENV), Utilities.removeProgramKeyAndUnknownAdditionalData(study.getStudyName(), program.getKey()));
@@ -1806,7 +1806,7 @@ public class ExperimentFileImportTest extends BrAPITest {
         assertEquals(expected.get(Columns.EXP_TITLE), Utilities.removeProgramKey(trial.getTrialName(), program.getKey()));
         assertEquals(expected.get(Columns.EXP_TITLE), Utilities.removeProgramKey(study.getTrialName(), program.getKey()));
         assertEquals(expected.get(Columns.EXP_DESCRIPTION), trial.getTrialDescription());
-        assertEquals(expected.get(Columns.EXP_UNIT), ou.getObservationUnitPosition().getObservationLevel().getLevelName());
+        assertEquals(expected.get(Columns.EXP_UNIT).toString().toLowerCase(), ou.getObservationUnitPosition().getObservationLevel().getLevelName().toLowerCase());
         assertEquals(expected.get(Columns.EXP_TYPE), trial.getAdditionalInfo().get(BrAPIAdditionalInfoFields.EXPERIMENT_TYPE).getAsString());
         assertEquals(expected.get(Columns.EXP_TYPE), study.getStudyType());
         assertEquals(expected.get(Columns.ENV), Utilities.removeProgramKeyAndUnknownAdditionalData(study.getStudyName(), program.getKey()));
